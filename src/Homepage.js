627cef6e1c0bae85c1dbd9fe2115e91ce8b9073e
Nathan Nichols <nwnichols02@gmail.com>
2022-02-25 15:08:41 -0700

    made styling updates meets mvp

--- src/Homepage.js
+++ src/Homepage.js
@@ -12,13 +12,7 @@ function Homepage () {
             <h3>Your favorite food, delivered while coding.</h3>
             <button id='order-pizza' onClick={pizzaRoute}>Pizza?</button>
         </center>
-        </section> 
-        <section id="six-images" className="bgimg">
-        <div>
-           <h4>blah blah blah add later</h4>
-        </div>
-
-        </section> 
+        </section>  
         </div>
     )
 }        
